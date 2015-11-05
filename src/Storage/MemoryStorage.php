878f99bde36ae79a8a954e2d30a4d15d10f06540
Vytautas Stankus <svycka@gmail.com>
2015-11-05 17:50:14 +0200

    added missing packages

---
+++ src/Storage/MemoryStorage.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Svycka\SettingsTest\TestAssets;
+
+use Svycka\Settings\Collection\CollectionInterface;
+use Svycka\Settings\Entity\SettingInterface;
+use Svycka\Settings\Storage\StorageAdapterInterface;
+
+class MemoryStorage implements StorageAdapterInterface
+{
+    protected $storage = [];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get(CollectionInterface $collection, $identifier, $name)
+    {
+        $options = $collection->getOptions();
+        if (isset($this->storage[$options->getName()][$identifier][$name])) {
+            $objectClass = $options->getObjectClass();
+            /** @var SettingInterface $setting */
+            $setting = new $objectClass;
+            $setting->setCollection($options->getName());
+            $setting->setIdentifier($identifier);
+            $setting->setName($name);
+            $setting->setValue($this->storage[$options->getName()][$identifier][$name]);
+            return $setting;
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function set(CollectionInterface $collection, $identifier, $name, $value)
+    {
+        $options = $collection->getOptions();
+        $this->storage[$options->getName()][$identifier][$name] = $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getList(CollectionInterface $collection, $identifier)
+    {
+
+        $options = $collection->getOptions();
+        $storedSettings = $this->storage[$options->getName()];
+        $objectClass = $options->getObjectClass();
+        $settings = [];
+
+        foreach ($storedSettings as $owner => $item) {
+            if ($owner !== $identifier) {
+                continue;
+            }
+
+            foreach ($item as $name => $value) {
+                /** @var SettingInterface $setting */
+                $setting = new $objectClass;
+                $setting->setCollection($options->getName());
+                $setting->setIdentifier($identifier);
+                $setting->setName($name);
+                $setting->setValue($value);
+                $settings[] = $setting;
+            }
+        }
+
+        return $settings;
+    }
+}
