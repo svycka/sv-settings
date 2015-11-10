5c6faadc2df49b42db51e1d4c72471e407a4f4c5
Vytautas Stankus <svycka@gmail.com>
2015-11-10 15:21:54 +0200

    added tests for abstract setting type class

---
+++ tests/Settings/Type/AbstractSettingTypeTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Svycka\SettingsTest\Type;
+
+use Svycka\Settings\Exception\InvalidArgumentException;
+use Svycka\Settings\Type\AbstractSettingType;
+use TestAssets\CustomAbstractSettingType;
+
+/**
+ * @author Vytautas Stankus <svycka@gmail.com>
+ * @license MIT
+ */
+class AbstractSettingTypeTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var CustomAbstractSettingType */
+    private $type;
+
+    public function setUp()
+    {
+        $this->type = new CustomAbstractSettingType;
+    }
+
+    public function testCanSetOptionsThroughConstructor()
+    {
+        $type = new CustomAbstractSettingType($options = ['options']);
+        $this->assertSame($options, $type->getOptions());
+    }
+
+    public function testCanSetGetOptions()
+    {
+        $this->type->setOptions($options = ['options']);
+        $this->assertSame($options, $this->type->getOptions());
+    }
+
+    public function testWillThrowExceptionIfInvalidOptionsProvided()
+    {
+        $this->setExpectedException(
+            InvalidArgumentException::class,
+            sprintf('%s::setOptions expects an array', AbstractSettingType::class)
+        );
+
+        $this->type->setOptions(new \stdClass());
+    }
+}
